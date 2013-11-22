1fe89f0a89c81b7f94e3dbcbf68fd13c440af04a
Apprentice <anniesing1014@gmail.com>
2013-11-21 21:18:36 -0800

    add strategies for solving problem

---
+++ largest_prime_factor.rb
@@ -0,0 +1,13 @@
+# Goal: Problem 3 from Euler Project
+# What is the largest prime factor of the number 600851475143?
+
+
+strategy:
+prime number: num that is only divisible by 1 and itself
+factor: a number that divides evenly into the multiple
+
+so, looking fo factors of 600851475143 that happen to be prime.
+
+1.) find all prime numbers up to 600851475143, then check to see if each 
+one divides evenly into 600851475143
+2.) find all factors of 600851475143, then check which ones are prime.
