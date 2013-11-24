e30e2845e45f3701a0d22763355eba61766025ef
Annie Sing <anniesing1014@gmail.com>
2013-11-24 03:00:12 -0800

    refactor factorfinder object, separate concerns of findFactors method and findLargestFactor method

--- largest_prime_factor.js
+++ largest_prime_factor.js
@@ -1,24 +1,26 @@
 function FactorFinder(num){
   this.num = num;
-  this.factors = [];
   this.findFactors = function(){
+    factors = [];
     for(var i = 2; i < this.num; i++){
       if(this.num%i == 0){
-        this.factors[this.factors.length] = i;
+        factors[factors.length] = i;
         this.num /= i;
       }
     }
     if(this.num !== 1){
-      this.factors[this.factors.length] = i;
+      factors[factors.length] = i;
     }
-    this.largestFactor = this.factors[this.factors.length-1];
+    return factors;
+  },
+  this.findLargestFactor = function(){
+    var factors = this.findFactors();
+    console.log(factors[factors.length - 1]);
   }
 }
 
-var myNum = new FactorFinder(10);
-myNum.findFactors();
-console.log(myNum.largestFactor);
+var question1 = new FactorFinder(10);
+question1.findLargestFactor();
 
-var myOtherNum = new FactorFinder(600851475143);
-myOtherNum.findFactors();
-console.log(myOtherNum.largestFactor);
+var question2 = new FactorFinder(600851475143);
+question2.findLargestFactor();
