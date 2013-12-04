37451411a733016cf0ae0b24ea6c5f8e22841d80
Annie Sing <anniesing1014@gmail.com>
2013-12-03 23:14:35 -0800

    attempt at making into module in hopes of not having too deep a stack level

--- smallest_multiple.rb
+++ smallest_multiple.rb
@@ -23,46 +23,52 @@
 #   - it may take too long to run this thing recursively.
 #   - running through 1-20 on every single number may take way too long also
 
+stack level was too deep once it started running beyond the range of 1-10, so I am going to try to make a module in an attempt to not repeat certain operations, intead saving results in instance variables.
+
 def assert(arg1, arg2)
   puts arg1 == arg2
 end
 
+module Multiple
 
-def divides_evenly?(dividend, divisor)
-  return dividend % divisor == 0
+  def divides_evenly?(dividend, divisor)
+    return dividend % divisor == 0
+  end
+
+  def all_factors_divide_evenly?(num, divisors)
+    no = []
+    divisors.each do |divisor|
+      if divides_evenly?(num, divisor) == false
+        return false
+      end
+    end
+    true
+  end
+
+  def find_smallest_multiple(num=nil, range)
+    divisors = (range).to_a
+    num ||= divisors.last
+    if all_factors_divide_evenly?(num, divisors)
+      return num
+    else
+      num += 1
+      find_smallest_multiple(num, range)
+    end
+  end
+
 end
 
+
+include Multiple
 assert(divides_evenly?(4,2), true)
 assert(divides_evenly?(4,7), false)
 assert(divides_evenly?(4,1), true)
-
-def all_factors_divide_evenly?(num, divisors)
-  no = []
-  divisors.each do |divisor|
-    if divides_evenly?(num, divisor) == false
-      return false
-    end
-  end
-  true
-end
 
 assert(all_factors_divide_evenly?(5, [1,2,3,4]), false)
 assert(all_factors_divide_evenly?(8, [1,2,4,8]), true)
 assert(all_factors_divide_evenly?(3, [1,2]), false)
 
-
-def find_smallest_multiple(num=nil, range)
-  divisors = (range).to_a
-  num ||= divisors.last
-  if all_factors_divide_evenly?(num, divisors)
-    return num
-  else
-    num += 1
-    find_smallest_multiple(num, range)
-  end
-end
-
 assert(find_smallest_multiple(1..3),6)
 assert(find_smallest_multiple(1..2),2)
-# puts find_smallest_multiple(1..10) => 2520
+assert(find_smallest_multiple(1..10), 2520)
 # puts find_smallest_multiple(1..11) => stack level too deep
