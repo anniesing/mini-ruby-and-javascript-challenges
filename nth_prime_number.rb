3e97b0d12da132dc984b80460437943f809da1b1
Annie Sing <anniesing1014@gmail.com>
2013-12-12 11:51:34 -0800

    add method for finding list of prime numbers, telling whether a number is prime or not, and modified way of finding exact prime number needed in #value

--- nth_prime_number.rb
+++ nth_prime_number.rb
@@ -1,8 +1,32 @@
 # Goal: What is the 10,001st prime number?
 
 class NthPrimeNumber
+  def initialize(n)
+    @n = n
+  end
 
+  def prime?(num)
+    return false if num == 1
+    return true if num == 2
+    for i in 2..num-1 do
+      return false if num % i == 0
+    end
+    true
+  end
 
+  def prime_numbers
+    primes = []
+    current_num = 2
+    until primes.length == @n do
+      primes << current_num if prime?(current_num)
+      current_num += 1
+    end
+    primes
+  end
+
+  def value
+    prime_numbers.last
+  end
 end
 
 
@@ -13,3 +37,9 @@ end
 
 sixth_prime = NthPrimeNumber.new(6)
 assert(sixth_prime.value, 13)
+assert(sixth_prime.prime_numbers, [2,3,5,7,11,13])
+assert(sixth_prime.prime?(2), true)
+assert(sixth_prime.prime?(3), true)
+assert(sixth_prime.prime?(1), false)
+assert(sixth_prime.prime?(4), false)
+assert(sixth_prime.prime?(10), false)
