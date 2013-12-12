f9fb220a9586b79b75cddf80b01ba62dcd47cb0b
Annie Sing <anniesing1014@gmail.com>
2013-12-12 10:51:26 -0800

    add method that sums of all squares of numbers leading up to given initial number

--- sum_square_difference.rb
+++ sum_square_difference.rb
@@ -1,4 +1,11 @@
 class SumSquareDifference
+  def initialize(num)
+    @num = num
+  end
+
+  def square_of_sum
+    (1..@num).inject { |sum, n| sum + n**2 }
+  end
 
   def difference
     square_of_sum - sum_of_squares
@@ -11,5 +18,7 @@ def assert(arg_1, arg_2)
 end
 
 
-first_10 = SumSquareDifference(10)
+first_10 = SumSquareDifference.new(10)
 assert(first_10.difference, 2640)
+
+assert(first_10.square_of_sum,385)
