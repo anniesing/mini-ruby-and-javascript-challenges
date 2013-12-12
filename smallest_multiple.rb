ebbebd1f0184e541c70a562e9de01f5ae31fea7d
Annie Sing <anniesing1014@gmail.com>
2013-12-12 10:36:08 -0800

    get rid of extra whitespace

--- smallest_multiple.rb
+++ smallest_multiple.rb
@@ -27,14 +27,12 @@ class Multiple
       @num += @highest_divisor
     end
   end
-
 end
 
 
 def assert(arg1, arg2)
   puts arg1 == arg2
 end
-
 
 test_1 = Multiple.new(1..3)
 assert(test_1.find_smallest_multiple, 6)
@@ -43,7 +41,6 @@ assert(test_2.find_smallest_multiple, 2)
 
 test_3 = Multiple.new(1..10)
 assert(test_3.find_smallest_multiple, 2520)
-
 
 start_time = Time.now
 semi_final_test = Multiple.new(1..20)
